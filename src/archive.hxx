4b8396f3ddbdcf35516ed2e1ab1e844e384adbf4
sangwan.kwon <sangwan.kwon@samsung.com>
2018-08-07 14:39:01 +0900

    Add archive class for serialization/parameter-pack
    - Add archival interface for serializable object.
    - The raw pointer type doesn't supported.
      (Because archive can not know whether the pointer is valid or not.)
    - transform() can be transform from archive to tuple.
    
    Signed-off-by: sangwan.kwon <sangwan.kwon@samsung.com>

---
+++ src/archive.hxx
@@ -0,0 +1,236 @@
+/*
+ *  Copyright (c) 2018 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/*
+ * @file        archive.hxx
+ * @author      Sangwan Kwon (sangwan.kwon@samsung.com)
+ * @brief       Define archive interface for serializer and parameter-pack.
+ * @details     1. Serializer: Serialize/deserialize below types.
+ *                 (fundamental types, archival object, unique_ptr, shared_ptr)
+ *              2. Parameter-pack: Pack/unpack zero or more template arguments.
+ */
+
+#pragma once
+
+#include <string>
+#include <type_traits>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <memory>
+#include <tuple>
+
+#include "index-sequence.hxx"
+
+namespace rmi {
+
+class Archival;
+
+template<typename T>
+using IsFundamental = typename std::enable_if<std::is_fundamental<T>::value, int>::type;
+
+template<typename T>
+using IsArchival = typename std::enable_if<std::is_base_of<Archival, T>::value, int>::type;
+
+class Archive {
+public:
+	virtual ~Archive() = default;
+
+	template<typename Front, typename... Rest>
+	void pack(const Front& front, const Rest&... rest);
+	inline void pack(void);
+
+	template<typename Front, typename... Rest>
+	void unpack(Front& front, Rest&... rest);
+	inline void unpack(void);
+
+	template<typename... Ts>
+	void transform(std::tuple<Ts...>& tuple);
+
+	template<typename T, IsFundamental<T> = 0>
+	Archive& operator<<(const T& value);
+	template<typename T, IsArchival<T> = 0>
+	Archive& operator<<(const T& object);
+	template<typename T>
+	Archive& operator<<(const std::unique_ptr<T>& pointer);
+	template<typename T>
+	Archive& operator<<(const std::shared_ptr<T>& pointer);
+	inline Archive& operator<<(const std::string& value);
+
+	template<typename T, IsFundamental<T> = 0>
+	Archive& operator>>(T& value);
+	template<typename T, IsArchival<T> = 0>
+	Archive& operator>>(T& object);
+	template<typename T>
+	Archive& operator>>(std::unique_ptr<T>& pointer);
+	template<typename T>
+	Archive& operator>>(std::shared_ptr<T>& pointer);
+	inline Archive& operator>>(std::string& value);
+
+protected:
+	virtual inline void save(const void* bytes, size_t size);
+	virtual inline void load(void* bytes, size_t size);
+
+private:
+	template<typename T, std::size_t... I>
+	void transformImpl(T& tuple, IndexSequence<I...>);
+
+	std::vector<unsigned char> buffer;
+
+	size_t current = 0;
+};
+
+class Archival {
+public:
+	virtual ~Archival() = default;
+
+	virtual void pack(Archive& archive) const = 0;
+	virtual void unpack(Archive& archive) = 0;
+};
+
+void Archive::pack(void)
+{
+}
+
+template<typename Front, typename... Rest>
+void Archive::pack(const Front& front, const Rest&... rest)
+{
+	*this << front;
+	this->pack(rest...);
+}
+
+void Archive::unpack(void)
+{
+}
+
+template<typename Front, typename... Rest>
+void Archive::unpack(Front& front, Rest&... rest)
+{
+	*this >> front;
+	this->unpack(rest...);
+}
+
+template<typename... Ts>
+void Archive::transform(std::tuple<Ts...>& tuple)
+{
+	constexpr auto size = std::tuple_size<std::tuple<Ts...>>::value;
+	this->transformImpl(tuple, make_index_sequence<size>());
+}
+
+template<typename T, std::size_t... I>
+void Archive::transformImpl(T& tuple, IndexSequence<I...>)
+{
+	this->unpack(std::get<I>(tuple)...);
+}
+
+template<typename T, IsFundamental<T>>
+Archive& Archive::operator<<(const T& value)
+{
+	this->save(reinterpret_cast<const void*>(&value), sizeof(value));
+
+	return *this;
+}
+
+template<typename T>
+Archive& Archive::operator<<(const std::unique_ptr<T>& pointer)
+{
+	return *this << *pointer;
+}
+
+template<typename T>
+Archive& Archive::operator<<(const std::shared_ptr<T>& pointer)
+{
+	return *this << *pointer;
+}
+
+template<typename T, IsArchival<T>>
+Archive& Archive::operator<<(const T& object)
+{
+	object.pack(*this);
+
+	return *this;
+}
+
+inline Archive& Archive::operator<<(const std::string& value)
+{
+	size_t size = value.size();
+	this->save(reinterpret_cast<const void*>(&size), sizeof(size));
+
+	this->save(reinterpret_cast<const void*>(value.c_str()), value.size());
+
+	return *this;
+}
+
+template<typename T, IsFundamental<T>>
+Archive& Archive::operator>>(T& value)
+{
+	this->load(reinterpret_cast<void*>(&value), sizeof(value));
+
+	return *this;
+}
+
+template<typename T>
+Archive& Archive::operator>>(std::unique_ptr<T>& pointer)
+{
+	if (pointer == nullptr)
+		pointer.reset(new T);
+
+	return *this >> *pointer;
+}
+
+template<typename T>
+Archive& Archive::operator>>(std::shared_ptr<T>& pointer)
+{
+	if (pointer == nullptr)
+		pointer.reset(new T);
+
+	return *this >> *pointer;
+}
+
+template<typename T, IsArchival<T>>
+Archive& Archive::operator>>(T& object)
+{
+	object.unpack(*this);
+
+	return *this;
+}
+
+inline Archive& Archive::operator>>(std::string& value)
+{
+	size_t size;
+	this->load(reinterpret_cast<void*>(&size), sizeof(size));
+
+	value.resize(size);
+	this->load(reinterpret_cast<void*>(&value.front()), size);
+
+	return *this;
+}
+
+void Archive::save(const void* bytes, size_t size)
+{
+	auto binary = reinterpret_cast<unsigned char*>(const_cast<void*>(bytes));
+	std::vector<unsigned char> next(binary, binary + size) ;
+
+	std::copy(next.begin(), next.end(), std::back_inserter(this->buffer));
+}
+
+void Archive::load(void* bytes, size_t size)
+{
+	::memcpy(bytes, reinterpret_cast<void*>(this->buffer.data() + current), size);
+	current += size;
+}
+
+} // namespace rmi
---
+++ test/test-archive.cpp
@@ -0,0 +1,286 @@
+/*
+ *  Copyright (c) 2018 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/*
+ * @file        test-archive.cpp
+ * @author      Sangwan Kwon (sangwan.kwon@samsung.com)
+ */
+
+#include "archive.hxx"
+
+#include <klay/testbench.h>
+
+#include <memory>
+#include <limits>
+#include <string>
+#include <cassert>
+#include <iostream>
+
+using namespace rmi;
+
+TESTCASE(SERIALIZER_FUNDAMENTAL_INT)
+{
+	int input1 = std::numeric_limits<int>::lowest();
+	int input2 = std::numeric_limits<int>::max();
+
+	Archive archive;
+	archive << input1 << input2;
+
+	int output1, output2;
+	archive >> output1 >> output2;
+
+	TEST_EXPECT(input1, output1);
+	TEST_EXPECT(input2, output2);
+}
+
+TESTCASE(SERIALIZER_FUNDAMENTAL_FLOAT)
+{
+	float input1 = std::numeric_limits<float>::lowest();
+	float input2 = std::numeric_limits<float>::max();
+
+	Archive archive;
+	archive << input1 << input2;
+
+	float output1, output2;
+	archive >> output1 >> output2;
+
+	TEST_EXPECT(input1, output1);
+	TEST_EXPECT(input2, output2);
+}
+
+TESTCASE(SERIALIZER_FUNDAMENTAL_DOUBLE)
+{
+	double input1 = std::numeric_limits<double>::lowest();
+	double input2 = std::numeric_limits<double>::max();
+
+	Archive archive;
+	archive << input1 << input2;
+
+	double output1, output2;
+	archive >> output1 >> output2;
+
+	TEST_EXPECT(input1, output1);
+	TEST_EXPECT(input2, output2);
+}
+
+TESTCASE(SERIALIZER_FUNDAMENTAL_LONG_LONG_INT)
+{
+	long long int input1 = std::numeric_limits<long long int>::lowest();
+	long long int input2 = std::numeric_limits<long long int>::max();
+
+	Archive archive;
+	archive << input1 << input2;
+
+	long long int output1, output2;
+	archive >> output1 >> output2;
+
+	TEST_EXPECT(input1, output1);
+	TEST_EXPECT(input2, output2);
+}
+
+TESTCASE(SERIALIZER_FUNDAMENTAL_BOOL)
+{
+	bool input1 = true;
+	bool input2 = false;
+
+	Archive archive;
+	archive << input1 << input2;
+
+	bool output1, output2;
+	archive >> output1 >> output2;
+
+	TEST_EXPECT(input1, output1);
+	TEST_EXPECT(input2, output2);
+}
+
+TESTCASE(SERIALIZER_FUNDAMENTAL_CHAR)
+{
+	char input1 = 'a';
+	char input2 = 'Z';
+
+	Archive archive;
+	archive << input1 << input2;
+
+	char output1, output2;
+	archive >> output1 >> output2;
+
+	TEST_EXPECT(input1, output1);
+	TEST_EXPECT(input2, output2);
+}
+
+TESTCASE(SERIALIZER_FUNDAMENTAL_NULLPTR)
+{
+	std::nullptr_t input;
+
+	Archive archive;
+	archive << input;
+
+	std::nullptr_t output;
+	archive >> output;
+
+	assert(input == output);
+}
+
+TESTCASE(SERIALIZER_FUNDAMENTAL_REFERENCE)
+{
+	int input = std::numeric_limits<int>::max();
+	int& refInput = input;
+
+	Archive archive;
+	archive << refInput;
+
+	int output;
+	int& refOutput = output;
+	archive >> refOutput;
+
+	TEST_EXPECT(input, output);
+	TEST_EXPECT(refInput, refOutput);
+}
+
+struct Object : public Archival {
+	void pack(Archive& archive) const override
+	{
+		archive << a << b << c;
+	}
+
+	void unpack(Archive& archive) override
+	{
+		archive >> a >> b >> c;
+	}
+
+	int a = 0;
+	bool b = true;
+	std::string c = "str";
+};
+
+TESTCASE(ARCHIVAL)
+{
+	Object input;
+	input.a = 100;
+	input.b = false;
+	input.c = "archival string";
+
+	Archive archive;
+	archive << input;
+
+	Object output;
+	archive >> output;
+
+	TEST_EXPECT(input.a, output.a);
+	TEST_EXPECT(input.b, output.b);
+	TEST_EXPECT(input.c, output.c);
+}
+
+TESTCASE(STRING)
+{
+	std::string input = "Archive string test";
+
+	Archive archive;
+	archive << input;
+
+	std::string output;
+	archive >> output;
+
+	TEST_EXPECT(input, output);
+}
+
+TESTCASE(UNIQUE_PTR)
+{
+	std::unique_ptr<bool> input(new bool(true));
+
+	Archive archive;
+	archive << input;
+
+	std::unique_ptr<bool> output;
+	archive >> output;
+
+	TEST_EXPECT(*input, *output);
+}
+
+TESTCASE(SHARED_PTR)
+{
+	std::shared_ptr<std::string> input = std::make_shared<std::string>();
+	*input = "Archive string test";
+
+	Archive archive;
+	archive << input;
+
+	std::shared_ptr<std::string> output;
+	archive >> output;
+
+	TEST_EXPECT(*input, *output);
+}
+
+TESTCASE(PARAMETER_PACK)
+{
+	int input1 = std::numeric_limits<int>::max();
+	float input2 = std::numeric_limits<float>::max();
+	double input3 = std::numeric_limits<double>::max();
+	std::string input4 = "Archive string test";
+	std::unique_ptr<bool> input5(new bool(true));
+
+	Archive archive;
+	archive.pack(input1, input2, input3, input4, input5);
+
+	int output1;
+	float output2;
+	double output3;
+	std::string output4;
+	std::unique_ptr<bool> output5;
+
+	archive.unpack(output1, output2, output3, output4, output5);
+
+	TEST_EXPECT(input1, output1);
+	TEST_EXPECT(input2, output2);
+	TEST_EXPECT(input3, output3);
+	TEST_EXPECT(input4, output4);
+	TEST_EXPECT(*input5, *output5);
+}
+
+TESTCASE(PARAMETER_PACK_EMPTY)
+{
+	Archive archive;
+	archive.pack();
+
+	archive.unpack();
+}
+
+TESTCASE(PARAMETER_PACK_TRANSFORM)
+{
+	int input1 = std::numeric_limits<int>::max();
+	float input2 = std::numeric_limits<float>::max();
+	double input3 = std::numeric_limits<double>::max();
+	std::string input4 = "Archive string test";
+	std::shared_ptr<bool> input5(new bool(true));
+
+	Archive archive;
+	archive.pack(input1, input2, input3, input4, input5);
+
+	int output1;
+	float output2;
+	double output3;
+	std::string raw;
+	std::string& output4 = raw;
+	std::shared_ptr<bool> output5;
+
+	auto tuple = std::make_tuple(output1, output2, output3, output4, output5);
+	archive.transform(tuple);
+
+	TEST_EXPECT(input1, std::get<0>(tuple));
+	TEST_EXPECT(input2, std::get<1>(tuple));
+	TEST_EXPECT(input3, std::get<2>(tuple));
+	TEST_EXPECT(input4, std::get<3>(tuple));
+	TEST_EXPECT(*input5, *(std::get<4>(tuple)));
+}
